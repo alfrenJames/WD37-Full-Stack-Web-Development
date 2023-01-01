ebd6b312a95a8d1ecfab2fb6e65ffbc7fd1d9eb7
jceroma23 <119847368+jceroma23@users.noreply.github.com>
2023-01-01 12:03:42 +0800

    Delete HandsOn/Ceroma Jestoni/html Hands on 2 directory

--- HandsOn/Ceroma Jestoni/html Hands on 2/vscode/signup.js
+++
@@ -1,8 +0,0 @@
-const formVar = document.getElementById('formSignUp');
-
-function validateform(){
-    var x = formVar.fName.value;
-    var y = formVar.lastName.value;
-    alert('You have Successfuly Register ' + x + ' ' + y);
-    return false;
-} 
