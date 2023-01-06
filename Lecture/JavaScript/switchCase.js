9095d031b41159e6b13260ada680623c659aa88c
Alfren James <103904239+alfrenJames@users.noreply.github.com>
2023-01-06 14:40:38 +0800

    Create switchCase.js

---
+++ Lecture/JavaScript/switchCase.js
@@ -0,0 +1,14 @@
+level=1;
+switch(level){
+    case 1 :
+    console.log('easy stage');
+    break;
+    case 2:
+    console.log('medium stage');
+    break;
+    case 3:
+    console.log('hard stage');
+    break;
+    default:
+    console.log('invalid stage');    
+}
