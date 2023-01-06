37f8d47ae88e5ea6a4771533ffcd9187c3c526d5
Alfren James <103904239+alfrenJames@users.noreply.github.com>
2023-01-06 14:41:03 +0800

    Create tenaryOperator.js

---
+++ Lecture/JavaScript/tenaryOperator.js
@@ -0,0 +1,9 @@
+var grade = 74;
+// if(grade>=75){
+//     console.log('passed'); //1
+// }else{
+//     console.log('failed'); //0
+// }
+//using tenary operator
+var output = (grade>=75) ? "passed":"failed";
+console.log(output);
