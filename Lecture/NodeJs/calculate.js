d69796be4563c373d35fda18c427effa8b64229d
Alfren James <103904239+alfrenJames@users.noreply.github.com>
2023-01-24 18:07:21 +0800

    Create calculate.js

---
+++ Lecture/NodeJs/calculate.js
@@ -0,0 +1,9 @@
+function add(fNum,sNum){
+    let sum = fNum + sNum;
+    console.log(sum);
+}
+function sub(fNum,sNum){
+    let diff = fNum - sNum;
+    console.log(diff);
+}
+module.exports = sub;
