ee6ef9a810fa44c9a54b7a1f3d80a34523ad28d0
Alfren James <103904239+alfrenJames@users.noreply.github.com>
2023-02-07 10:20:59 +0800

    Create addRecord.js

---
+++ Lecture/NodeJs/Express/addRecord.js
@@ -0,0 +1,18 @@
+//import model
+//import json file 
+
+//functio to add record 
+// const insertMOvies = async()=>{
+//     try {
+          //Movie is the Model
+          //movies is the json file that you will insert to Mongo DB
+//         const add = await Movie.insertMany(movies);
+//         return Promise.resolve(add);
+//     } catch (error) {
+//         return Promise.reject(error);
+//     }
+// }
+
+// insertMOvies()
+// .then((addMovie)=>console.log(addMovie))
+// .catch((err=>console.log(err)));
