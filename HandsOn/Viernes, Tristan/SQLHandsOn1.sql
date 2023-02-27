e13324470c3975df1a3b909555b555106b748592
Tristan Viernes <119847375+trztn@users.noreply.github.com>
2023-02-27 18:30:50 +0800

    Add files via upload

---
+++ HandsOn/Viernes, Tristan/SQLHandsOn1.sql
@@ -0,0 +1,130 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 27, 2023 at 11:25 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `record`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `DeptID` int(11) NOT NULL,
+  `DepName` varchar(100) DEFAULT NULL,
+  `Location` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`DeptID`, `DepName`, `Location`) VALUES
+(1001, 'Executive', 'BGC'),
+(1002, 'Hr', 'Mandaluyong'),
+(1003, 'IT', 'Bulacan'),
+(1004, 'Sales', 'Y Ami'),
+(1005, 'Marketing', 'Jolo');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `EmpNo` int(11) NOT NULL,
+  `EmpName` varchar(100) DEFAULT NULL,
+  `Job` varchar(100) DEFAULT NULL,
+  `Mgr` int(11) DEFAULT NULL,
+  `HireDate` datetime DEFAULT NULL,
+  `Salary` decimal(10,0) DEFAULT NULL,
+  `Commission` int(11) DEFAULT NULL,
+  `DeptID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`EmpNo`, `EmpName`, `Job`, `Mgr`, `HireDate`, `Salary`, `Commission`, `DeptID`) VALUES
+(1001, 'Scott', 'President', NULL, '1978-01-01 00:00:00', 5000, NULL, 1001),
+(1058, 'Clark', 'Manager', 1001, '1978-01-01 00:00:00', 4000, NULL, 1001),
+(1059, 'Jeff', 'Manager', 1001, '1978-01-01 00:00:00', 3500, 500, 1002),
+(1060, 'Marko', 'Manager', 1001, '1978-01-01 00:00:00', 4000, NULL, 1003),
+(1065, 'Bryan', 'Salesman', 1060, '1979-02-04 00:00:00', 3000, 300, 1002),
+(1066, 'Frauline', 'Salesman', 1060, '1978-02-08 00:00:00', 4000, 600, 1002),
+(1067, 'Phylip', 'Salesman', 1060, '1978-04-15 00:00:00', 3300, 0, 1002),
+(1068, 'Ejerson', 'Clerk', 1060, '1978-01-15 00:00:00', 2400, NULL, 1002),
+(1069, 'Julie', 'Clerk', 1065, '1983-05-12 00:00:00', 2000, NULL, 1003),
+(1070, 'Russel', 'Analyst', 1065, '1979-03-01 00:00:00', 3400, NULL, 1003),
+(1071, 'Buboy', 'Analyst', 1065, '1979-03-01 00:00:00', 3600, NULL, 1003),
+(1072, 'Joseph', 'Analyst', 1065, '1981-01-09 00:00:00', 3100, NULL, 1003);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`DeptID`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`EmpNo`),
+  ADD KEY `FK_Mgr` (`Mgr`),
+  ADD KEY `FK_DeptID` (`DeptID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `DeptID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1007;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `EmpNo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1073;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `employee`
+--
+ALTER TABLE `employee`
+  ADD CONSTRAINT `FK_DeptID` FOREIGN KEY (`DeptID`) REFERENCES `department` (`DeptID`),
+  ADD CONSTRAINT `FK_Mgr` FOREIGN KEY (`Mgr`) REFERENCES `employee` (`EmpNo`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
