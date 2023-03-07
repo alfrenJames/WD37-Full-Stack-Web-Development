c1981a53724cd7fbed1b885bf3347abef332a417
Alfren James <103904239+alfrenJames@users.noreply.github.com>
2023-03-07 13:38:51 +0800

    Create addRecord.inc.php

---
+++ Lecture/PHP/OOPDemo/includes/addRecord.inc.php
@@ -0,0 +1,7 @@
+<?php
+    $record = new employeeRecord;
+    $record->set_employee_record("John Rick", "WebDev", "Micro Soft", 434536.45, "image.png");
+    $record->get_employee_record();
+    $record->set_employee_record("John Paul", "WebDev", "Micro Soft", 434536.45, "image.png");
+    $record->get_employee_record();
+?>
