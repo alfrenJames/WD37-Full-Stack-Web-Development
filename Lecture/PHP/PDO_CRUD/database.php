adb0f81503dc85b5edb33fd469dc7d392b9a4525
Alfren James <103904239+alfrenJames@users.noreply.github.com>
2023-03-09 14:43:38 +0800

    Create database.php

---
+++ Lecture/PHP/PDO_CRUD/database.php
@@ -0,0 +1,17 @@
+<?php
+if(!defined("DB_TYPE")){
+    define("DB_TYPE", "mysql");
+}
+if(!defined("DB_HOST")){
+    define("DB_HOST", "localhost");
+}
+if(!defined("DB_NAME")){
+    define("DB_NAME", "registration");
+}
+if(!defined("DB_PWD")){
+    define("DB_PWD", "");
+}
+if(!defined("DB_USER")){
+    define("DB_USER", "root");
+}
+?>
