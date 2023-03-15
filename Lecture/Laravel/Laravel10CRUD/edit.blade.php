a3daee5b81b36128114c069a998410a42e3784bb
Alfren James <103904239+alfrenJames@users.noreply.github.com>
2023-03-16 07:23:45 +0800

    Create edit.blade.php

---
+++ Lecture/Laravel/Laravel10CRUD/edit.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Edit Company Form - Laravel 10 CRUD Tutorial</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+</head>
+<body>
+<div class="container mt-2">
+<div class="row">
+<div class="col-lg-12 margin-tb">
+<div class="pull-left">
+<h2>Edit Company</h2>
+</div>
+<div class="pull-right">
+<a class="btn btn-primary" href="{{ route('companies.index') }}" enctype="multipart/form-data"> Back</a>
+</div>
+</div>
+</div>
+@if(session('status'))
+<div class="alert alert-success mb-1 mt-1">
+{{ session('status') }}
+</div>
+@endif
+<form action="{{ route('companies.update',$company->id) }}" method="POST" enctype="multipart/form-data">
+@csrf
+@method('PUT')
+<div class="row">
+<div class="col-xs-12 col-sm-12 col-md-12">
+<div class="form-group">
+<strong>Company Name:</strong>
+<input type="text" name="name" value="{{ $company->name }}" class="form-control" placeholder="Company name">
+@error('name')
+<div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+@enderror
+</div>
+</div>
+<div class="col-xs-12 col-sm-12 col-md-12">
+<div class="form-group">
+<strong>Company Email:</strong>
+<input type="email" name="email" class="form-control" placeholder="Company Email" value="{{ $company->email }}">
+@error('email')
+<div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+@enderror
+</div>
+</div>
+<div class="col-xs-12 col-sm-12 col-md-12">
+<div class="form-group">
+<strong>Company Address:</strong>
+<input type="text" name="address" value="{{ $company->address }}" class="form-control" placeholder="Company Address">
+@error('address')
+<div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+@enderror
+</div>
+</div>
+<button type="submit" class="btn btn-primary ml-3">Submit</button>
+</div>
+</form>
+</div>
+</body>
+</html>
