575a1aecb8517f29c8677c80f4b32e6c895cc47e
Alfren James <103904239+alfrenJames@users.noreply.github.com>
2023-03-16 13:53:10 +0800

    Update edit.blade.php

--- Lecture/Laravel/Laravel10CRUD/edit.blade.php
+++ Lecture/Laravel/Laravel10CRUD/edit.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<title>Edit Company Form - Laravel 10 CRUD Tutorial</title>
+<title>Edit Form - Laravel 10 CRUD Tutorial</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
 </head>
 <body>
@@ -10,10 +10,10 @@
 <div class="row">
 <div class="col-lg-12 margin-tb">
 <div class="pull-left">
-<h2>Edit Company</h2>
+<h2>Edit Employee</h2>
 </div>
 <div class="pull-right">
-<a class="btn btn-primary" href="{{ route('companies.index') }}" enctype="multipart/form-data"> Back</a>
+<a class="btn btn-primary" href="/employees" enctype="multipart/form-data"> Back</a>
 </div>
 </div>
 </div>
@@ -22,37 +22,56 @@
 {{ session('status') }}
 </div>
 @endif
-<form action="{{ route('companies.update',$company->id) }}" method="POST" enctype="multipart/form-data">
+<form action="{{ route('employees.update',$employee->id) }}" method="POST" enctype="multipart/form-data">
 @csrf
 @method('PUT')
 <div class="row">
 <div class="col-xs-12 col-sm-12 col-md-12">
 <div class="form-group">
 <strong>Company Name:</strong>
-<input type="text" name="name" value="{{ $company->name }}" class="form-control" placeholder="Company name">
+<input type="text" name="name" value="{{ $employee->name }}" class="form-control" placeholder="Company name">
 @error('name')
 <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
 @enderror
 </div>
 </div>
 <div class="col-xs-12 col-sm-12 col-md-12">
+    <div class="form-group">
+    <strong>Photo Url:</strong>
+    <input type="text" name="photo" value="{{ $employee->photo }}" class="form-control" placeholder="Company name">
+    @error('name')
+    <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+    @enderror
+    </div>
+    </div>
+<div class="col-xs-12 col-sm-12 col-md-12">
 <div class="form-group">
 <strong>Company Email:</strong>
-<input type="email" name="email" class="form-control" placeholder="Company Email" value="{{ $company->email }}">
+<input type="email" name="email" class="form-control" placeholder="Company Email" value="{{ $employee->email }}">
 @error('email')
 <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
 @enderror
 </div>
 </div>
 <div class="col-xs-12 col-sm-12 col-md-12">
-<div class="form-group">
-<strong>Company Address:</strong>
-<input type="text" name="address" value="{{ $company->address }}" class="form-control" placeholder="Company Address">
-@error('address')
-<div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-@enderror
+    <div class="form-group">
+    <strong>Job Title:</strong>
+    <input type="text" name="jobTitle" value="{{ $employee->jobTitle }}" class="form-control" placeholder="Company name">
+    @error('name')
+    <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+    @enderror
+    </div>
 </div>
+<div class="col-xs-12 col-sm-12 col-md-12">
+    <div class="form-group">
+    <strong>Salary:</strong>
+    <input type="text" name="salary" value="{{ $employee->salary }}" class="form-control">
+    @error('name')
+    <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+    @enderror
+    </div>
 </div>
+
 <button type="submit" class="btn btn-primary ml-3">Submit</button>
 </div>
 </form>
